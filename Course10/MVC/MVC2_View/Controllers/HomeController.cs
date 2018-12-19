d90702d6218b064cd87e5acc1037c2f1bb0008ea
Erdinç Uzun <erdincuzun@msn.com>
2018-12-19 10:30:33 +0300

    Add files via upload

---
+++ Course10/MVC/MVC2_View/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC2.Models;
+
+namespace MVC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ParametreliSayfa(string par1, int kackere = 5)
+        {
+            ViewData["metin"] = "Hello " + par1;
+            ViewData["kackere"] = kackere;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
