ebee5a45009c5a9dcd46762e6b66a326b4d94b27
Erdinç Uzun <erdincuzun@msn.com>
2018-12-19 10:26:25 +0300

    Add files via upload

---
+++ Course10/MVC/MVC1_Controller/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(){
+            return "Merhaba My Controller";
+        }
+
+        public string Sayfa2(){
+            return "Sayfa 2 çağrıldı";
+        }
+
+        public string ParametreliSayfa(string par1, string par2="MVC2"){
+            return HtmlEncoder.Default.Encode($"1: {par1}, 2: {par2}");
+        }
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+
+        // public IActionResult Privacy()
+        // {
+        //     return View();
+        // }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
